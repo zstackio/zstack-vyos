67c09ae23cdd3191cca45cf2b90728b28850825d
Frank Zhang <xing5820@gmail.com>
2016-10-18 00:17:31 +0800

    working on vyos parser

--- src/zvr/vyos_parser.go
+++ src/zvr/vyos_parser.go
@@ -1,3 +1,160 @@
 package zvr
 
+import (
+	"strings"
+	"bufio"
+	"github.com/pkg/errors"
+	"fmt"
+	"zvr/utils"
+)
+
+type VyosParser struct {
+	data map[string]interface{}
+	parsed bool
+}
+
+type role int
+const (
+	ROOT role = iota
+	ROOT_ATTRIBUTE
+	KEY_VALUE
+	CLOSE
+	IGNORE
+)
+
+func matchToken(words []string) (int, role, []string, string) {
+	ws := make([]string, 0)
+	next := 0
+
+	// find until \n
+	for next = 0; next < len(words); next++ {
+		w := words[next]
+		if w == "\n" {
+			break
+		}
+
+		ws = append(ws, w)
+	}
+
+	length := len(ws)
+	if length == 2 && ws[length-1] == "{" {
+		return next, ROOT, []string{ws[0]}, ""
+	} else if  length > 2 && ws[length-1] == "{" {
+		return next, ROOT_ATTRIBUTE, ws[:length-1], ""
+	} else if length == 2 && ws[length-1] != "{" && ws[length-1] != "}" {
+		return next, KEY_VALUE, []string{ws[0]}, ws[1]
+	} else if length == 1 && ws[0] == "}" {
+		return next, CLOSE, nil, ""
+	} else if length == 0 {
+		return next+1, IGNORE, nil, ""
+	} else {
+		panic(errors.New(fmt.Sprintf("unable to parser the words: %s", strings.Join(words, " "))))
+	}
+}
+
+type VyosConfig struct {
+	data map[string]interface{}
+}
+
+func (c *VyosConfig) GetValue(keys ...string) (string, bool) {
+	var current interface{} = c.data
+	for _, key := range keys {
+		m, ok := current.(map[string]interface{})
+		if !ok {
+			return "", false
+		}
+
+		current = m[key]
+		if current == nil {
+			return "", false
+		}
+	}
+
+	if v, ok := current.(string); ok {
+		return v, true
+	} else {
+		return "", false
+	}
+}
+
+func (c *VyosConfig) GetConfig(keys ...string) (*VyosConfig, bool) {
+	var current interface{} = c.data
+	for _, key := range keys {
+		m, ok := current.(map[string]interface{})
+		if !ok {
+			return nil, false
+		}
+
+		current = m[key]
+		if current == nil {
+			return nil, false
+		}
+	}
+
+	if m, ok := current.(map[string]interface{}); ok {
+		return &VyosConfig{
+			data: m,
+		}, true
+	} else {
+		return nil, false
+	}
+}
+
+func (parser *VyosParser) GetValue(keys ...string) (string, bool) {
+	if (len(keys) == 1) {
+		utils.Assert(parser.parsed, "you must call Parse() before GetValue()")
+
+	}
+
+	mainKeys := keys[:len(keys)-1]
+	if c, ok := parser.GetConfig(mainKeys...); ok {
+		return c.GetValue([]string{keys[len(keys)-1]}...)
+	} else {
+		return "", false
+	}
+}
+
+func (parser *VyosParser) GetConfig(keys ...string) (*VyosConfig, bool) {
+	utils.Assert(parser.parsed, "you must call Parse() before GetConfig()")
+
+	c := VyosConfig{ data: parser.data}
+	return c.GetConfig(keys...)
+}
+
+func (parser *VyosParser) Parse(text string) {
+	parser.parsed = true
+
+	words := make([]string, 0)
+	for _, s := range strings.Split(text, "\n") {
+		scanner := bufio.NewScanner(strings.NewReader(s))
+		scanner.Split(bufio.ScanWords)
+		ws := make([]string, 0)
+		for scanner.Scan() {
+			ws = append(ws, scanner.Text())
+		}
+		ws = append(ws, "\n")
+		words = append(words, ws...)
+	}
+
+	offset := 0
+	parser.data = make(map[string]interface{})
+	var current map[string]interface{}
+	for i := 0; i < len(words); i += offset {
+		o, role, keys, value := matchToken(words[i:])
+		offset = o
+		if role == ROOT {
+			current = make(map[string]interface{})
+			parser.data[keys[0]] = current
+		} else if role == KEY_VALUE {
+			current[keys[0]] = value
+		} else if role == ROOT_ATTRIBUTE {
+			for _, key := range keys {
+				current[key] = make(map[string]interface{})
+				current = current[key].(map[string]interface{})
+			}
+		} else if role == CLOSE {
+			current = make(map[string]interface{})
+		}
+	}
+}
 
--- src/zvr/vyos_parser_test.go
+++ src/zvr/vyos_parser_test.go
@@ -2,246 +2,106 @@ package zvr
 
 import (
 	"testing"
-	"bufio"
-	"strings"
 	"fmt"
-	"unicode/utf8"
-	"github.com/pkg/errors"
+	"zvr/utils"
 )
 
-func isSpace(r rune) bool {
-	if r <= '\u00FF' {
-		// Obvious ASCII ones: \t through \r plus space. Plus two Latin-1 oddballs.
-		switch r {
-		case ' ', '\t', '\v', '\f', '\r':
-			return true
-		case '\u0085', '\u00A0':
-			return true
-		}
-		return false
-	}
-	// High-valued ones.
-	if '\u2000' <= r && r <= '\u200a' {
-		return true
-	}
-	switch r {
-	case '\u1680', '\u2028', '\u2029', '\u202f', '\u205f', '\u3000':
-		return true
-	}
-	return false
-}
-
-func isSpace1(r byte) bool {
-	// Obvious ASCII ones: \t through \r plus space. Plus two Latin-1 oddballs.
-	switch r {
-	case ' ', '\n', '\t', '\v', '\f', '\r':
-		return true
-	case '\u0085', '\u00A0':
-		return true
-	}
-	return false
-}
-
-func scanWords1(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	start := 0
-	dataLen := len(data)
-
-	for i := 0; i < dataLen; i ++ {
-		w := data[i]
-		if w == '\n' || !isSpace1(w) {
-			break
-		}
-		start ++
-	}
-
-	for i := start; i < dataLen; i ++ {
-		w := data[i]
-		if w == '\n' {
-			return i+1, data[start:i+1], nil
-		}
-
-		if isSpace1(w) {
-			return i, data[start:i], nil
-		}
-	}
-
-	if dataLen > start && atEOF {
-		return dataLen, data[start:], nil
-	}
-
-	// require more data
-	return start, nil, nil
-}
-
-func scanWords(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	//fmt.Printf("data: %s\n", string(data))
-	// Skip leading spaces.
-	start := 0
-	for width := 0; start < len(data); start += width {
-		var r rune
-		r, width = utf8.DecodeRune(data[start:])
-		if !isSpace(r) {
-			break
-		}
-	}
-	//fmt.Printf("data1: %s\n", string(data))
-	// Scan until space, marking end of word.
-	for width, i := 0, start; i < len(data); i += width {
-		var r rune
-		r, width = utf8.DecodeRune(data[i:])
-		if isSpace(r) {
-			return i + width, data[start:i], nil
-		}
-	}
-	// If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
-	if atEOF && len(data) > start {
-		return len(data), data[start:], nil
-	}
-	// Request more data.
-	return start, nil, nil
-}
-
-type role int
-const (
-	ROOT role = iota
-	ROOT_ATTRIBUTE
-	KEY_VALUE
-	IGNORE
-)
-
-func find(words []string) (offset int, role role, key, value string) {
-	ws := make([]string, 0)
-	next := 0
-
-	// find until \n
-	for next = 0; next < len(words); next++ {
-		w := words[next]
-		if w == "\n" {
-			break
-		}
-
-		ws = append(ws, w)
-	}
-
-	//next ++
-	length := len(ws)
-	fmt.Printf("xxxxxxxxxxxxxxxxxxxxxxxxx %v %v\n", ws, next)
-	if length == 2 && ws[length-1] == "{" {
-		return next, ROOT, ws[0], ""
-	} else if  length > 2 && ws[length-1] == "{" {
-		return next, ROOT_ATTRIBUTE, strings.Join(ws, "."), ""
-	} else if length == 2 && ws[length-1] != "{" && ws[length-1] != "}" {
-		return next, KEY_VALUE, ws[0], ws[1]
-	} else if length == 1 && ws[0] == "}" {
-		return next, IGNORE, "", ""
-	} else if length == 0 {
-		return next+1, IGNORE, "", ""
-	} else {
-		panic(errors.New(strings.Join(words, " ")))
-	}
-}
-
-func TestVyosParser(t *testing.T) {
+func TestVyosParser1(t *testing.T) {
 	text := `
-	interfaces {
-		ethernet eth0 {
-		address dhcp
-		hw-id 00:0c:29:6a:ef:80
-	}
-		loopback lo {
-	}
-	}
-	service {
-		ssh {
-			port 22
-		}
-	}
-	system {
-		config-management {
-			commit-revisions 20
-		}
-		console {
-		device ttyS0 {
-		speed 9600
-		}
-		}
-		login {
-		user vyos {
-		authentication {
-		encrypted-password ****************
-		plaintext-password ****************
-		}
-		level admin
-		}
-		}
-		ntp {
-		server 0.pool.ntp.org {
-		}
-		server 1.pool.ntp.org {
-		}
-		server 2.pool.ntp.org {
-		}
-		}
-		package {
-		repository community {
-		components main
-		distribution helium
-		url http://packages.vyos.net/vyos
-		}
-		}
-		syslog {
-		global {
-		facility all {
-		level notice
-		}
-		facility protocols {
-		level debug
-		}
-		}
-		}
-	}
-`
-
-	/*
-	split := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-		advance, token, err = bufio.ScanWords(data, atEOF)
-		if err == nil && token != nil {
-			//fmt.Println(string(token))
-		}
-		return
-	}
-	*/
-
-	words := make([]string, 0)
-	for _, s := range strings.Split(text, "\n") {
-		scanner := bufio.NewScanner(strings.NewReader(s))
-		scanner.Split(bufio.ScanWords)
-		ws := make([]string, 0)
-		for scanner.Scan() {
-			ws = append(ws, scanner.Text())
-		}
-		ws = append(ws, "\n")
-		words = append(words, ws...)
-	}
-
-	offset := 0
-	for i := 0; i < len(words); i += offset {
-		fmt.Printf("yyyyyyyyyyyyyyyyyyyyyyy %v\n", i)
-		o, role, key, value := find(words[i:])
-		offset = o
-		fmt.Printf("offset: %d, role: %d, key: %v, value: %v\n", offset, role, key, value)
-	}
-
-	/*
-	scanner := bufio.NewScanner(strings.NewReader(text))
-	//scanner.Split(split)
-	scanner.Split(scanWords)
-	words := make([]string, 0)
-	for scanner.Scan() {
-		words = append(words, scanner.Text())
-	}
-	for _, w := range words {
-		fmt.Printf("word: %s\n", w)
-	}
-	*/
+interfaces {
+    ethernet eth0 {
+        address 172.20.14.209/16
+        description main
+        duplex auto
+        hw-id fa:da:21:1f:1a:00
+        smp_affinity auto
+        speed auto
+    }
+    loopback lo {
+    }
 }
+protocols {
+    static {
+        route 0.0.0.0/0 {
+            next-hop 172.20.0.1 {
+                distance 1
+            }
+        }
+    }
+}
+service {
+    ssh {
+        port 22
+    }
+}
+system {
+    config-management {
+        commit-revisions 20
+    }
+    console {
+        device ttyS0 {
+            speed 9600
+        }
+    }
+    host-name vyos
+    login {
+        user vyos {
+            authentication {
+                encrypted-password ****************
+                plaintext-password ****************
+            }
+            level admin
+        }
+    }
+    ntp {
+        server 0.pool.ntp.org {
+        }
+        server 1.pool.ntp.org {
+        }
+        server 2.pool.ntp.org {
+        }
+    }
+    package {
+        auto-sync 1
+        repository community {
+            components main
+            distribution helium
+            password ****************
+            url http://packages.vyos.net/vyos
+            username ""
+        }
+    }
+    syslog {
+        global {
+            facility all {
+                level notice
+            }
+            facility protocols {
+                level debug
+            }
+        }
+    }
+    time-zone UTC
+}
+`
+	p := VyosParser{}
+	p.Parse(text)
+	addr, ok := p.GetValue("interfaces", "ethernet", "eth0", "address")
+	utils.Assert(ok, "fail")
+	fmt.Println(addr)
+
+	c, ok := p.GetConfig("interfaces", "ethernet", "eth0")
+	utils.Assert(ok, "fail")
+
+	addr, ok = c.GetValue("address")
+	utils.Assert(ok, "fail")
+	fmt.Println(addr)
+
+	c, ok = p.GetConfig("interfaces", "ethernet", "eth0", "Asdfasdf")
+	utils.Assert(!ok, "fail")
+
+	value, ok := p.GetValue("time-zone")
+	utils.Assert(ok, "fail")
+	fmt.Println(value)
+}
+
