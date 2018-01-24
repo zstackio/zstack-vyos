4305b36a8b763848f85fdc055402e998210b858b
Wei Wang <wei.wang@mevoco.com>
2018-01-24 14:55:36 +0800

    Fix install-zvrboot.sh
    Avoid it will always overwrite vyatta-postconfig-bootup.script

--- scripts/install-zvrboot.sh
+++ scripts/install-zvrboot.sh
@@ -15,6 +15,6 @@ chmod +x $TARGET_BIN
 
 mkdir -p /home/vyos/zvr
 chown vyos:users /home/vyos/zvr
-echo "/sbin/zvrboot >/home/vyos/zvr/zvrboot.log 2>&1 < /dev/null" > /opt/vyatta/etc/config/scripts/vyatta-postconfig-bootup.script
+grep -E 'zvrboot.*zvrboot.log' /opt/vyatta/etc/config/scripts/vyatta-postconfig-bootup.script || echo "/sbin/zvrboot >/home/vyos/zvr/zvrboot.log 2>&1 < /dev/null" >> /opt/vyatta/etc/config/scripts/vyatta-postconfig-bootup.script
 
 exit 0
