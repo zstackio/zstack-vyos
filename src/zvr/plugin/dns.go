b08d9ef4f5ee2c7671b766c73cfeb25b45b93dee
Ruan Shixin <shixin.ruan@zstack.io>
2018-05-16 22:32:57 +0800

    Add vpc dns feature

--- src/zvr/plugin/dns.go
+++ src/zvr/plugin/dns.go
@@ -9,6 +9,7 @@ import (
 const (
 	REMOVE_DNS_PATH = "/removedns"
 	SET_DNS_PATH = "/setdns"
+	SET_VPNDNS_PATH = "/setvpcdns"
 )
 
 type dnsInfo struct {
@@ -22,6 +23,11 @@ type setDnsCmd struct {
 
 type removeDnsCmd struct {
 	Dns []dnsInfo `json:"dns"`
+}
+
+type setVpcDnsCmd struct {
+	Dns []string `json:"dns"`
+	NicMac []string `json:"nicMac"`
 }
 
 
@@ -89,7 +95,64 @@ func removeDnsHandler(ctx *server.CommandContext) interface{} {
 	return nil
 }
 
+func setVpcDnsHandler(ctx *server.CommandContext) interface{} {
+	tree := server.NewParserFromShowConfiguration().Tree
+
+	cmd := &setVpcDnsCmd{}
+	ctx.GetCommand(cmd)
+
+	/* remove old dns  */
+	tree.Deletef("service dns")
+	priNics := utils.GetPrivteInterface()
+	for _, priNic := range priNics {
+		des := makeDnsFirewallRuleDescription(priNic)
+		if r := tree.FindFirewallRuleByDescription(priNic, "local", des); r != nil {
+			r.Delete()
+		}
+	}
+
+	if (len(cmd.Dns) == 0 || len(cmd.NicMac) == 0) {
+		tree.Apply(false)
+		return nil
+	}
+
+	/* add new configure */
+	var nics []string
+	for _, mac := range cmd.NicMac{
+		eth, err := utils.GetNicNameByMac(mac); utils.PanicOnError(err)
+		nics = append(nics, eth)
+	}
+	if (len(nics) == 0) {
+		tree.Apply(false)
+		return nil
+	}
+
+	for _, nic := range nics {
+		tree.SetfWithoutCheckExisting("service dns forwarding listen-on %s", nic)
+
+		des := makeDnsFirewallRuleDescription(nic)
+		if r := tree.FindFirewallRuleByDescription(nic, "local", des); r == nil {
+			tree.SetFirewallOnInterface(nic, "local",
+				fmt.Sprintf("description %v", des),
+				"destination port 53",
+				"protocol tcp_udp",
+				"action accept",
+			)
+
+			tree.AttachFirewallToInterface(nic, "local")
+		}
+	}
+
+	for _, dns := range cmd.Dns {
+		tree.SetfWithoutCheckExisting("service dns forwarding name-server %s", dns)
+	}
+
+	tree.Apply(false)
+	return nil
+}
+
 func DnsEntryPoint() {
 	server.RegisterAsyncCommandHandler(SET_DNS_PATH, server.VyosLock(setDnsHandler))
 	server.RegisterAsyncCommandHandler(REMOVE_DNS_PATH, server.VyosLock(removeDnsHandler))
+	server.RegisterAsyncCommandHandler(SET_VPNDNS_PATH, server.VyosLock(setVpcDnsHandler))
 }
