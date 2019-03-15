9d93b8d94ebef048ef510a32a52bc0fc5b3871fd
zhanyong.miao <zhanyong.miao@zstack.io>
2019-03-15 10:58:21 +0800

    support the ospf for ZSTAC-18376

---
+++ src/zvr/plugin/ospf.go
@@ -0,0 +1,280 @@
+package plugin
+
+import (
+	"zvr/server"
+	"zvr/utils"
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/pkg/errors"
+	"strings"
+	"unicode"
+)
+
+const (
+	ROUTER_PROTOCOL_REFRESH_OSPF = "/routerprotocol/ospf/refresh"
+	ROUTER_PROTOCOL_GET_OSPF_NEIGHBOR = "/routerprotocol/ospf/neighbor"
+
+	FIREWALL_DESCRIPTION="ospf-firewall"
+)
+
+type RouterAreaType string
+type RouterAreaAuthType string
+const (
+	Standard RouterAreaType = "Standard"
+	Stub 	 RouterAreaType = "Stub"
+	NSSA 	 RouterAreaType = "NSSA"
+)
+const (
+	None 	  RouterAreaAuthType = "None"
+	MD5 	  RouterAreaAuthType = "MD5"
+	Plaintext RouterAreaAuthType = "Plaintext"
+)
+
+type areaInfo struct {
+	AreaId string `json:"areaId"`
+	AreaType RouterAreaType `json:"type"`
+	AuthType RouterAreaAuthType `json:"authType"`
+	AuthParam string `json:"authParam"`
+}
+
+type networkInfo struct {
+	NicMac string `json:"nicMac"`
+	Network string `json:"network"`
+	AreaId string `json:"areaId"`
+}
+
+type setOspfCmd struct {
+	RouterId string `json:"routerId"`
+	AreaInfos []areaInfo `json:"areaInfos"`
+	NetworkInfos []networkInfo `json:"networkInfos"`
+}
+
+type neighbor struct {
+	Id string  `json:"id"`
+	Priority string `json:"priority"`
+	State string `json:"state"`
+	DeadTime string `json:"deadTime"`
+	NeighborAddress string `json:"neighborAddress"`
+	Device string `json:"device"`
+}
+type getOspfNeighborRsp struct {
+	Neighbors []neighbor `json:"neighbors"`
+}
+
+type protocol interface {
+	init(*server.VyosConfigTree) (err error)
+	setRouterId()  ( err error)
+	setArea() ( error)
+	setNetwork() ( err error)
+	setRawCmd(f string, args...interface{}) (err error)
+	delRawCmd(f string, args...interface{}) (err error)
+	getNeighbors() ([]neighbor, error)
+	commit() (err error)
+}
+
+// the ospf protocol implement
+type ospfProtocol struct {
+	Id string
+	AreaInfos []areaInfo
+	NetworkInfos []networkInfo
+	Tree *server.VyosConfigTree
+}
+
+func (this *ospfProtocol) init(tree *server.VyosConfigTree)  ( err error) {
+	err = nil
+	this.Tree = tree;
+	return err;
+}
+
+func (this *ospfProtocol) setRouterId()  ( err error) {
+	if this.Tree == nil {
+		panic(fmt.Errorf("missing initial"))
+	}
+	this.Tree.Setf("protocols ospf parameters router-id %s", this.Id)
+
+	return nil;
+}
+
+func (this *ospfProtocol) setArea()  ( err error) {
+	if this.Tree == nil {
+		panic(fmt.Errorf("missing initial.."))
+	}
+	for _, info := range this.AreaInfos {
+		if info.AreaType == Standard {
+			this.Tree.Setf("protocols ospf area %s area-type %s", info.AreaId, "normal")
+		} else if info.AreaType == NSSA {
+			this.Tree.Setf("protocols ospf area %s area-type %s", info.AreaId, "nssa")
+		} else {
+			this.Tree.Setf("protocols ospf area %s area-type %s", info.AreaId, "stub")
+		}
+
+		if info.AuthType == Plaintext {
+			this.Tree.Setf("protocols ospf area %s authentication plaintext-password", info.AreaId)
+		} else if info.AuthType == MD5 {
+			this.Tree.Setf("protocols ospf area %s authentication md5", info.AreaId)
+		}
+	}
+	return nil;
+}
+
+func (this *ospfProtocol) setNetwork()  (err error) {
+	if this.Tree == nil {
+		panic(fmt.Errorf("missing initial.."))
+	}
+
+	for _, info := range this.AreaInfos {
+		for _, n := range this.NetworkInfos {
+			if n.AreaId != info.AreaId {
+				continue;
+			}
+			nic, err := utils.GetNicNameByMac(n.NicMac); utils.PanicOnError(err)
+			this.Tree.Setf("protocols ospf area %s network %s", info.AreaId, n.Network)
+
+			if r := this.Tree.FindFirewallRuleByDescription(nic, "in", FIREWALL_DESCRIPTION); r == nil {
+				this.Tree.SetFirewallOnInterface(nic, "in",
+					fmt.Sprintf("description %v", FIREWALL_DESCRIPTION),
+					fmt.Sprintf("protocol ospf"),
+					"action accept",
+				)
+
+			}
+
+			if info.AuthType == Plaintext {
+				this.Tree.Setf("interfaces ethernet %s ip ospf authentication plaintext-password %s", nic, info.AuthParam)
+			} else if info.AuthType == MD5 {
+				this.Tree.Setf("interfaces ethernet %s ip ospf authentication md5 key-id %s", nic, info.AuthParam)
+			}
+
+		}
+		if info.AreaType == Standard {
+			this.Tree.Setf("protocols ospf area %s area-type %s", info.AreaId, "normal")
+		} else if info.AreaType == NSSA {
+			this.Tree.Setf("protocols ospf area %s area-type %s", info.AreaId, "nssa")
+		} else {
+			this.Tree.Setf("protocols ospf area %s area-type %s", info.AreaId, "stub")
+		}
+
+		if info.AuthType == Plaintext {
+			this.Tree.Setf("protocols ospf area %s authentication plaintext-password", info.AreaId)
+		} else if info.AuthType == MD5 {
+			this.Tree.Setf("protocols ospf area %s authentication md5", info.AreaId)
+		}
+	}
+	return err
+}
+
+func (this *ospfProtocol) setRawCmd(f string, args...interface{})  ( err error) {
+	if this.Tree == nil {
+		panic(fmt.Errorf("missing initial.."))
+	}
+	this.Tree.Setf(fmt.Sprintf(f, args...))
+	return err
+}
+
+func (this *ospfProtocol) delRawCmd(f string, args...interface{})  ( err error) {
+	if this.Tree == nil {
+		panic(fmt.Errorf("missing initial.."))
+	}
+	this.Tree.Deletef(fmt.Sprintf(f, args...))
+
+	return err
+}
+
+func (this *ospfProtocol) getNeighbors()  ( neighbors []neighbor,err error) {
+	return neighbors,err
+}
+
+func (this *ospfProtocol) commit() (err error) {
+	if this.Tree == nil {
+		panic(fmt.Errorf("missing initial.."))
+	}
+	this.Tree.Apply(false)
+	return nil
+}
+
+func getProtocol(id string, area []areaInfo, networks []networkInfo) protocol {
+	return &ospfProtocol{Id:id, AreaInfos:area, NetworkInfos:networks}
+}
+
+func refreshOspf(ctx *server.CommandContext) interface{} {
+	/*
+	1. if NetworkInfos null, deleteOspf else
+	2. remove the whole ospf configure
+	3. re-configure the ospf and commit
+	 */
+	cmd := &setOspfCmd{}
+	ctx.GetCommand(cmd)
+
+	log.Debugf(fmt.Sprintf("ospf refresh cmd for %v %v %v ", cmd.RouterId, cmd.AreaInfos, cmd.NetworkInfos))
+	p := getProtocol(cmd.RouterId, cmd.AreaInfos, cmd.NetworkInfos);
+	err := p.init(server.NewParserFromShowConfiguration().Tree); utils.PanicOnError(err)
+	err = p.delRawCmd("protocols ospf"); utils.PanicOnError(err)
+	if cmd.NetworkInfos != nil && len(cmd.NetworkInfos) > 0 {
+		err = p.setRouterId(); utils.PanicOnError(err)
+		err = p.setArea(); utils.PanicOnError(err)
+		err = p.setNetwork(); utils.PanicOnError(err)
+		err = p.setRawCmd("protocols ospf log-adjacency-changes"); utils.PanicOnError(err)
+	}
+
+	p.commit()
+	return nil
+}
+
+func parseNeighbor(input string) ( []neighbor,  error) {
+	neighbors := make([]neighbor,0,50)
+
+	if input =="" {
+		return neighbors, nil
+	}
+	rows := strings.Split(strings.Trim(input,"\\s+"), "\n")
+	for _, row := range rows {
+		if row == "" {
+			continue
+		}
+		columns := strings.FieldsFunc(strings.Trim(row, "\\s+"), unicode.IsSpace)
+		if len(columns) < 6 {
+			log.Debugf(fmt.Sprintf("rows: %v row:%v columns:%v", rows, row, len(columns)))
+
+			return nil, errors.Errorf("invalid neighbor string: %s", input)
+		}
+
+		neighbors = append(neighbors, neighbor{Id:columns[0], Priority:columns[1],State:columns[2],
+				DeadTime:columns[3], NeighborAddress:columns[4], Device:columns[5]})
+	}
+	return neighbors, nil
+}
+
+/*testneighbors :=  []neighbor{
+	neighbor{
+		Id:"1.1.1.1",
+		Priority:"5",
+		State:"Full/DR",
+		DeadTime:"40.88s",
+		NeighborAddress:"192.168.251.244",
+		Device:"eth2:192.168.251.12"},
+	neighbor{
+		Id:"1.1.2.1",
+		Priority:"5",
+		State:"Full/Backup",
+		DeadTime:"40.88s",
+		NeighborAddress:"192.168.252.244",
+		Device:"eth2:192.168.252.18"},
+}*/
+func getNeighbors(ctx *server.CommandContext) interface{} {
+	bash := utils.Bash {
+		Command: fmt.Sprintf("vtysh -c 'show ip ospf neighbor' | tail -n +3; vtysh -c 'show ip ospf neighbor' >/dev/null"),
+	}
+	ret, o, _, err := bash.RunWithReturn(); utils.PanicOnError(err)
+	if ret != 0 {
+		utils.PanicOnError(errors.Errorf(("get neighbor from zebra error")))
+	}
+
+	neighbors,err := parseNeighbor(o); utils.PanicOnError(err)
+
+	return getOspfNeighborRsp{Neighbors:neighbors}
+}
+
+func OspfEntryPoint()  {
+	server.RegisterAsyncCommandHandler(ROUTER_PROTOCOL_REFRESH_OSPF, refreshOspf)
+	server.RegisterAsyncCommandHandler(ROUTER_PROTOCOL_GET_OSPF_NEIGHBOR, getNeighbors)
+}
