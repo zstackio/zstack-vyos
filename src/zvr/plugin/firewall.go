b6fa1379cef36e1f07184d1ea8e00084d52205b3
yaohua.wu <pandawuu@163.com>
2019-08-15 21:13:21 +0800

    resolve ZSTAC-22733:move nic firewall config while reconnect

--- src/zvr/plugin/firewall.go
+++ src/zvr/plugin/firewall.go
@@ -299,6 +299,8 @@ func getFirewallConfig(ctx *server.CommandContext) interface{} {
 		panic(errors.New("can not use firewall if skipvyosiptables is true"))
 	}
 
+	tree := server.NewParserFromShowConfiguration().Tree
+
 	cmd := &getConfigCmd{}
 	ctx.GetCommand(cmd)
 	ethInfos := make([]ethInfo, 0)
@@ -320,7 +322,6 @@ func getFirewallConfig(ctx *server.CommandContext) interface{} {
 	}
 
 	//sync ruleSet and rules
-	tree := server.NewParserFromShowConfiguration().Tree
 	rs := tree.Get("firewall name")
 	rules := make([]ruleInfo, 0)
 	ruleSets := make([]ruleSetInfo, 0)
