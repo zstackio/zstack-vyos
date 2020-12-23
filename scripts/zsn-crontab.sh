9cd9be319a31c4d8d182d05f5a22d78802e6a089
shixin.ruan <shixin.ruan@zstack.io>
2020-12-23 13:43:10 +0800

    [BugFix: ZSTACK-35406]  save zsn state to temp file: /tmp/dr

--- scripts/zsn-crontab.sh
+++ scripts/zsn-crontab.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-bash /etc/init.d/zstack-network-agent status || bash /etc/init.d/zstack-network-agent start
+BOOTLOG=/home/vyos/zvr/zvrstartup.log
+sudo bash /etc/init.d/zstack-network-agent status
+ret=$?
+if [ $ret -ne 0 ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') start zstack network agent by vyos cron job" >>$BOOTLOG
+    sudo bash /etc/init.d/zstack-network-agent start
+fi
