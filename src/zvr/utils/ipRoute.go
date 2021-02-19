336140dd54f1b3d3f394f549e21411cc4aca1c65
shixin.ruan <shixin.ruan@zstack.io>
2021-02-19 21:25:39 +0800

    [BugFix: ZSTACK-37792] fix policy route reinstall when restart zvr

---
+++ src/zvr/utils/ipRoute.go
@@ -0,0 +1,267 @@
+package utils
+
+import (
+	"fmt"
+	"io/ioutil"
+	log "github.com/Sirupsen/logrus"
+	"strconv"
+	"strings"
+)
+
+const (
+	POLICY_ROUTE_TABLE_FILE      = "/etc/iproute2/rt_tables"
+	POLICY_ROUTE_TABLE_FILE_TEMP = "/home/vyos/zvr/.zs_rt_tables"
+	VYOSHA_POLICY_ROUTE_SCRIPT   = "/home/vyos/zvr/keepalived/script/policyRoutes.sh"
+)
+
+type ZStackRouteTable struct {
+	TableId int
+	Alias   string
+}
+
+func (t ZStackRouteTable) toString () string{
+	return fmt.Sprintf("%d %s", t.TableId, t.Alias)
+}
+
+func (t ZStackRouteTable) flushCommand () string{
+	return fmt.Sprintf("sudo ip route flush table %d", t.TableId)
+}
+
+func GetZStackRouteTables() []ZStackRouteTable {
+	var tables []ZStackRouteTable
+	content, err := ioutil.ReadFile(POLICY_ROUTE_TABLE_FILE)
+	if err != nil {
+		log.Debugf("read file: %s, failed: %s", POLICY_ROUTE_TABLE_FILE, err)
+		return tables
+	}
+
+	log.Debugf("%s contents: %s", POLICY_ROUTE_TABLE_FILE, content)
+	lines := strings.Split(strings.TrimSpace(string(content)), "\n")
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		if line[0] == '#' {
+			continue
+		}
+
+		items := strings.Fields(line)
+		if strings.Contains(items[1], PolicyRouteChainPrefix) {
+			tableId, _ := strconv.Atoi(items[0])
+			t := ZStackRouteTable{TableId: tableId, Alias: items[1]}
+			tables = append(tables, t)
+		}
+	}
+
+	return tables
+}
+
+func SyncZStackRouteTables(tables []ZStackRouteTable) error {
+	bash := Bash {
+		Command: fmt.Sprintf("cp %s %s; sed -i '/zs-rt-/d' %s", POLICY_ROUTE_TABLE_FILE, POLICY_ROUTE_TABLE_FILE_TEMP, POLICY_ROUTE_TABLE_FILE_TEMP),
+	}
+	ret, _, e, err := bash.RunWithReturn()
+	if err != nil || ret != 0 {
+		return fmt.Errorf("create temp route table file failed: %s", e)
+	}
+
+	var cmds []string
+	for _, t := range tables {
+		cmds = append(cmds, t.toString())
+	}
+	if len(cmds) > 0 {
+		bash = Bash{
+			Command: fmt.Sprintf("cat <<EOF >> %s \n%s \nEOF\n sudo mv %s %s", POLICY_ROUTE_TABLE_FILE_TEMP, strings.Join(cmds, "\n"),
+				POLICY_ROUTE_TABLE_FILE_TEMP, POLICY_ROUTE_TABLE_FILE),
+		}
+	} else {
+		bash = Bash{
+			Command: fmt.Sprintf("sudo mv %s %s", POLICY_ROUTE_TABLE_FILE_TEMP, POLICY_ROUTE_TABLE_FILE),
+		}
+	}
+	ret, _, e, err = bash.RunWithReturn()
+	if err != nil || ret != 0{
+		return fmt.Errorf("copy temp route table file failed: %s", e)
+	}
+
+	return nil
+}
+
+type ZStackRouteEntry struct {
+	TableId int
+	DestinationCidr string
+	NextHopIp string
+	NicName string
+	Distance int
+}
+
+func (e ZStackRouteEntry) isEqual(b ZStackRouteEntry) bool {
+	if e.NicName != "" {
+		return e.TableId == b.TableId && e.DestinationCidr == b.DestinationCidr && e.NicName == b.NicName && e.Distance == b.Distance
+	} else {
+		return e.TableId == b.TableId && e.DestinationCidr == b.DestinationCidr && e.NextHopIp == b.NextHopIp && e.Distance == b.Distance
+	}
+}
+
+func (e ZStackRouteEntry) addCommand() string {
+	if e.NicName == "" {
+		if e.Distance != 0 {
+			return fmt.Sprintf("sudo ip route add %s metric %d via %s table %d",
+				e.DestinationCidr, e.Distance, e.NextHopIp, e.TableId)
+		} else {
+			return fmt.Sprintf("sudo ip route add %s via %s table %d",
+				e.DestinationCidr, e.NextHopIp, e.TableId)
+		}
+	} else {
+		return fmt.Sprintf("sudo ip route add %s dev %s table %d",
+			e.DestinationCidr, e.NicName, e.TableId)
+	}
+}
+
+func (e ZStackRouteEntry) deleteCommand() string {
+	if e.NicName == "" {
+		if e.Distance != 0 {
+			return fmt.Sprintf("sudo ip route del %s metric %d via %s table %d",
+				e.DestinationCidr, e.Distance, e.NextHopIp, e.TableId)
+		} else {
+			return fmt.Sprintf("sudo ip route del %s via %s table %d",
+				e.DestinationCidr, e.NextHopIp, e.TableId)
+		}
+	} else {
+		return fmt.Sprintf("sudo ip route del %s dev %s table %d",
+			e.DestinationCidr, e.NicName, e.TableId)
+	}
+}
+
+func getCurrentRouteEntries(tableId int) []ZStackRouteEntry {
+	var entries []ZStackRouteEntry
+	bash := Bash {
+		Command: fmt.Sprintf("ip route show table %d", tableId),
+	}
+	ret, result, _, err := bash.RunWithReturn()
+	if err == nil && ret == 0{
+		result = strings.TrimSpace(result)
+		lines := strings.Split(result, "\n")
+		for _, line := range lines {
+			if line == "" {
+				continue
+			}
+
+			items := strings.Fields(line)
+			distance := 0
+			for i, item := range items {
+				if item == "metric" {
+					distance, _ = strconv.Atoi(items[i + 1])
+				}
+			}
+
+			if items[0] == "default" {
+				e := ZStackRouteEntry {
+					TableId: tableId,
+					DestinationCidr: "0.0.0.0/0",
+					NextHopIp: items[2],
+					Distance: distance,
+				}
+				entries = append(entries, e)
+			} else if items[1] == "via" {
+				e := ZStackRouteEntry {
+					TableId: tableId,
+					DestinationCidr: items[0],
+					NextHopIp: items[2],
+					Distance: distance,
+				}
+				entries = append(entries, e)
+			} else if items[1] == "dev" {
+				e := ZStackRouteEntry {
+					TableId: tableId,
+					DestinationCidr: items[0],
+					NicName: items[2],
+					Distance: distance,
+				}
+				entries = append(entries, e)
+			}
+		}
+	}
+
+	return entries
+}
+
+func SyncRouteEntries(currTables []ZStackRouteTable, entryMap map[int][]ZStackRouteEntry)  error {
+	var newCmds []string
+	/* delete route tables */
+	for _, table := range currTables {
+		if _, ok := entryMap[table.TableId]; !ok {
+			newCmds = append(newCmds, table.flushCommand())
+		}
+	}
+
+	for tableId, entries := range entryMap {
+		currEntries := getCurrentRouteEntries(tableId)
+		/* delete old entries that is not needed */
+		for _, oe := range currEntries {
+			exist := false
+			for _, ne := range entries {
+				if oe.isEqual(ne) {
+					exist = true
+					break
+				}
+			}
+
+			if !exist {
+				newCmds = append(newCmds, oe.deleteCommand())
+			}
+		}
+
+		/* add new entries that is added */
+		for _, ne := range entries {
+			exist := false
+			for _, oe := range currEntries {
+				if oe.isEqual(ne) {
+					exist = true
+					break
+				}
+			}
+
+			if !exist {
+				newCmds = append(newCmds, ne.addCommand())
+			}
+		}
+	}
+
+	if len(newCmds) == 0 {
+		return nil
+	}
+
+	bash := Bash {
+		Command: strings.Join(newCmds, ";"),
+	}
+	ret, _, e, err := bash.RunWithReturn()
+	if err != nil {
+		return err
+	}
+
+	if ret != 0 {
+		return fmt.Errorf("sync ip route: %s, error: %s, ret: %d", strings.Join(newCmds, ";"), e, ret)
+	}
+
+	return writePolicyRouteHaScript(entryMap)
+}
+
+func writePolicyRouteHaScript(entryMap map[int][]ZStackRouteEntry) error {
+	if !IsHaEnabled() {
+		return nil
+	}
+
+	var routes []string
+	for _, entries := range entryMap {
+		for _, ne := range entries {
+			routes = append(routes, ne.addCommand())
+		}
+	}
+
+	return ioutil.WriteFile(VYOSHA_POLICY_ROUTE_SCRIPT, []byte(strings.Join(routes, "\n")), 0755)
+}
+
+
