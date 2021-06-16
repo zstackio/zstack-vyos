9bec54d06277c7284777984cff45158b77fc1ff4
dengsong.yang <dengsong.yang@zstack.io>
2021-06-16 14:07:04 +0800

    [BugFix:ZSTACK-41104]

--- scripts/zvr-reboot.sh
+++ scripts/zvr-reboot.sh
@@ -12,6 +12,22 @@ restart_zvr() {
   sudo bash /etc/init.d/zstack-virtualrouteragent restart >> /tmp/agentRestart.log 2>&1
 }
 
+check_version() {
+    ret=$(timeout 3 curl -sb -H "Content-Type: application/json; charset=utf-8" -H "User-Agent: curl" -X POST $uri)
+    if [[ "$ret" =~ "\"success\":true" ]]; then
+        current_version=$(parse_json $ret "zvrVersion")
+        if [ x$zvr_version != x$current_version ];then
+            su - vyos -c "echo $(date '+%Y-%m-%d %H:%M:%S') the running zvr version is $current_version and management node zvr version is $zvr_version >> $LOGFILE"
+            result="restart"
+        else
+            su - vyos -c "echo $(date '+%Y-%m-%d %H:%M:%S') the running zvr version is $current_version and management node zvr version is $zvr_version, no need restart zstack virtual router >> $LOGFILE"
+            result="success"
+        fi
+    else
+        result="failed"
+    fi
+}
+
 manageNicIp=$(grep -A 5 "managementNic" $BOOTSTRAPINFO | grep "ip" | awk '{print $2}')
 if [ x$manageNicIp = x"" ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') get managementNic ip failed, restart zstack virtual router " >> $LOGFILE
@@ -40,17 +56,24 @@ if [ x$pid = x"" ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') zstack virtual router is stopped, restart zstack virtual router" >> $LOGFILE
     restart_zvr
 else
-    ret=$(timeout 5 curl -sb -H "Content-Type: application/json; charset=utf-8" -H "User-Agent: curl" -X POST $uri)
-    if [[ "$ret" =~ "\"success\":true" ]]; then
-        current_version=$(parse_json $ret "zvrVersion")
-        if [ x$zvr_version != x$current_version ];then
-            echo "$(date '+%Y-%m-%d %H:%M:%S') the running zvr version is $current_version and management node zvr version is $zvr_version, restart zstack virtual router" >> $LOGFILE
-            restart_zvr
+    retry=1
+    maxRetries=10
+    until [ ${retry} -ge ${maxRetries} ]
+    do
+        check_version
+        if [ x"$result" = x"failed" ]
+        then
+            echo "failed, retrying the $retry time"
+            su - vyos -c "$(date '+%Y-%m-%d %H:%M:%S') zstack virtual router pid: $pid, curl  $uri failed, retrying the $retry time >> $LOGFILE"
+            sleep $(( retry++ ))
         else
-            echo "$(date '+%Y-%m-%d %H:%M:%S') the running zvr version is $current_version and management node zvr version is $zvr_version, no need restart zstack virtual router" >> $LOGFILE
+            ((retry=maxRetries+1))
         fi
-    else
-        echo "$(date '+%Y-%m-%d %H:%M:%S') zstack virtual router pid: $pid, curl  $uri failed, $ret".restart zstack virtual router>> $LOGFILE
+    done
+
+    if [ x"$result" = x"restart" ]
+    then
+        echo "$(date '+%Y-%m-%d %H:%M:%S') running zvr version is different from management node zvr version, restart zstack virtual router" >> $LOGFILE
         restart_zvr
     fi
 fi
