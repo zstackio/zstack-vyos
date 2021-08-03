54fd48ac6e83ea1bc6baa656bfc3551086f22d74
dengsong.yang <dengsong.yang@zstack.io>
2021-08-03 13:36:24 +0800

    [BugFix:ZSTACK-41949]

--- plugin/plugin_suite_test.go
+++ plugin/plugin_suite_test.go
@@ -2,6 +2,7 @@ package plugin_test
 
 import (
 	"testing"
+	"zvr/utils"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -11,4 +12,8 @@ func TestPlugin(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Plugin Suite")
 }
+
+var _ = BeforeSuite(func() {
+    utils.InitForUt()
+})
 
