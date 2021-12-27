5e554d5b60be0cfc33664808375c5fc5c08b5705
dengsong.yang <dengsong.yang@zstack.io>
2021-12-27 15:17:02 +0800

    <fix>[upgrade]: fix not restart zvr when upgrade
    Resolves: ZSTAC-44568
    
    Change-Id: I656961747a737379626b6a667164667872626261

--- scripts/zvr-reboot.sh
+++ scripts/zvr-reboot.sh
@@ -3,24 +3,49 @@
 HOMDIR=/home/vyos/zvr
 LOGFILE=$HOMDIR/zvrReboot.log
 BOOTSTRAPINFO=$HOMDIR/bootstrap-info.json
+UPGRADE_VERSION_FILE="false"
+
+restart_zvr() {
+  if [ x"$UPGRADE_VERSION_FILE" = x"true" ]; then
+    cp $HOMDIR/mn_zvr_version $HOMDIR/version
+  fi
+  sudo bash /etc/init.d/zstack-virtualrouteragent restart >> /tmp/agentRestart.log 2>&1
+}
+
+# if not version file exist,zvr may not be started,restart it
+zvr_version=$(cat $HOMDIR/version)
+if [ x$zvr_version = x"" ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') get zvr version failed, restart zstack virtual router " >> $LOGFILE
+    restart_zvr
+    exit
+fi
+
+mn_zvr_version=$(cat $HOMDIR/mn_zvr_version)
+if [ x$mn_zvr_version = x"" ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') get mn zvr version failed, restart zstack virtual router " >> $LOGFILE
+    restart_zvr
+    exit
+else
+    if [ x$mn_zvr_version != x$zvr_version ]; then
+        UPGRADE_VERSION_FILE="true"
+        echo "$(date '+%Y-%m-%d %H:%M:%S') get mn zvr version $mn_zvr_version" >> $LOGFILE
+    fi
+fi
 
 parse_json(){
   echo "${1//\"/}" | sed "s/.*$2:\([^,}]*\).*/\1/"
 }
 
-restart_zvr() {
-  sudo bash /etc/init.d/zstack-virtualrouteragent restart >> /tmp/agentRestart.log 2>&1
-}
 
 check_version() {
     ret=$(timeout 30 curl -s -H "Content-Type: application/json; charset=utf-8" -H "User-Agent: curl" -X POST $uri)
     if [[ "$ret" =~ "\"success\":true" ]]; then
         current_version=$(parse_json $ret "zvrVersion")
-        if [ x$zvr_version != x$current_version ];then
-            su - vyos -c "echo $(date '+%Y-%m-%d %H:%M:%S') the running zvr version is $current_version and management node zvr version is $zvr_version >> $LOGFILE"
+        if [ x$mn_zvr_version != x$current_version ];then
+            echo "$(date '+%Y-%m-%d %H:%M:%S') the running zvr version is $current_version and management node zvr version is $mn_zvr_version" >> $LOGFILE
             result="restart"
         else
-            su - vyos -c "echo $(date '+%Y-%m-%d %H:%M:%S') the running zvr version is $current_version and management node zvr version is $zvr_version, no need restart zstack virtual router >> $LOGFILE"
+            echo "$(date '+%Y-%m-%d %H:%M:%S') the running zvr version is $current_version and management node zvr version is $mn_zvr_version, no need restart zstack virtual router" >> $LOGFILE
             result="success"
         fi
     elif [[ "$ret" =~ "no plugin registered the path" ]]; then ##old version image, there is no test command in zvr
@@ -44,13 +69,6 @@ if [ x$manageNicIp = x"" ]; then
     exit
 fi
 
-zvr_version=$(cat $HOMDIR/version)
-if [ x$zvr_version = x"" ]; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') get zvr version failed, restart zstack virtual router " >> $LOGFILE
-    restart_zvr
-    exit
-fi
-
 ##check zvr versoin
 uri=http://$manageNicIp:7272/test
 pid=$(ps aux | grep '/opt/vyatta/sbin/zvr' | grep -v grep | awk '{print $2}')
@@ -66,7 +84,7 @@ else
         if [ x"$result" = x"failed" ]
         then
             echo "failed, retrying the $retry time"
-            su - vyos -c "$(date '+%Y-%m-%d %H:%M:%S') zstack virtual router pid: $pid, curl  $uri failed, retrying the $retry time >> $LOGFILE"
+            echo "$(date '+%Y-%m-%d %H:%M:%S') zstack virtual router pid: $pid, curl  $uri failed, retrying the $retry time" >> $LOGFILE
             sleep $(( retry++ ))
         else
             ((retry=maxRetries+1))
