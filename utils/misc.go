91b606c749a33643556f47a775b1c156f49de58e
shixin.ruan <shixin.ruan@zstack.io>
2024-11-14 11:17:10 +0800

    <fix>[utils]: set vyos version same to mn guestOsType<description>
    Resolves: ZSTAC-70864
    
    Change-Id: I6a6f6d75617a657a7763766f6e696e6f67676a73

--- utils/misc.go
+++ utils/misc.go
@@ -13,12 +13,12 @@ import (
 type CompareStringFunc func(string, string) bool
 
 const (
-	VYOS_1_1_7      = "1.1.7"
-	VYOS_1_2        = "1.2"
+	VYOS_1_1_7      = "VyOS 1.1.7"
+	VYOS_1_2        = "VyOS 1.2.0"
 	TIME_ZONE_FILE  = "/etc/timezone"
 	LOCAL_TIME_FILE = "/etc/localtime"
 
-	EULER_22_03 = "openEuler release 22.03 "
+	EULER_22_03 = "openEuler 22.03 "
 )
 
 var (
@@ -26,7 +26,7 @@ var (
 	StringCompareFn    = CompareString
 	StringRegCompareFn = CompareRegString
 
-	Cronjob_file_zsn         = "/usr/local/zstack/zsn-agent/bin/zsn-crontab.sh"
+	Cronjob_file_zsn = "/usr/local/zstack/zsn-agent/bin/zsn-crontab.sh"
 
 	Vyos_version_file = "/opt/vyatta/etc/version"
 	Vyos_version      = VYOS_1_1_7
@@ -37,7 +37,6 @@ var (
 
 	Euler_version_file = "/etc/system-release"
 )
-
 
 func GetZtackConfigPath() string {
 	return filepath.Join(GetZvrRootPath(), ".zstack_config")
@@ -76,37 +75,37 @@ func CompareRegString(reg, src string) bool {
 }
 
 func InitVyosVersion() {
-	
+
 	Vyos_version = "unknown"
 	if ok, err := PathExists(Vyos_version_file); err == nil && ok {
 		/* $ cat /opt/vyatta/etc/version
-			Version:      VyOS 1.1.7
-			Description:  VyOS 1.1.7 (helium)
-			Copyright:    2016 VyOS maintainers and contributors
+		Version:      VyOS 1.1.7
+		Description:  VyOS 1.1.7 (helium)
+		Copyright:    2016 VyOS maintainers and contributors
 		*/
 		log.Debugf("read /opt/vyatta/etc/version")
 		if version, err := ReadLine(Vyos_version_file); err == nil {
 			log.Debugf("version: " + version)
-			if strings.Contains(version, VYOS_1_1_7) {
+			if strings.Contains(version, "1.1.7") {
 				Vyos_version = VYOS_1_1_7
-			} else if strings.Contains(version, VYOS_1_2) {
+			} else if strings.Contains(version, "1.2") {
 				Vyos_version = VYOS_1_2
 			}
 		}
 	} else if ok, err := PathExists(Euler_version_file); err == nil && ok {
 		/* # cat /etc/system-release
-			openEuler release 22.03 (LTS-SP3)
+		openEuler release 22.03 (LTS-SP3)
 		*/
 		log.Debugf("read /etc/system-release")
 		if version, err := ReadLine(Euler_version_file); err == nil {
 			log.Debugf("version: " + version)
-			if strings.Contains(version, EULER_22_03) {
+			if strings.Contains(version, "openEuler release 22.03") {
 				Vyos_version = EULER_22_03
-			} 
+			}
 		}
 	}
 	log.Debugf("zstack vyos version %s", Vyos_version)
-	
+
 	b := &Bash{
 		Command: "uname -r",
 	}
@@ -149,7 +148,7 @@ func SetTimeZone(timeZone string) error {
 			Command: fmt.Sprintf("timedatectl set-timezone %s", timeZone),
 			Sudo:    true,
 		}
-	
+
 		return bash.Run()
 	}
 
@@ -184,7 +183,7 @@ func ServiceOperation(name string, operation string) error {
 		command = fmt.Sprintf("/etc/init.d/%s %s", name, operation)
 	} else {
 		/* ntp service name on arm vyos is ntp */
-		if name == "ntpd"  && IsVYOS() {
+		if name == "ntpd" && IsVYOS() {
 			name = "ntp"
 		}
 		command = fmt.Sprintf("systemctl %s %s", operation, name)
